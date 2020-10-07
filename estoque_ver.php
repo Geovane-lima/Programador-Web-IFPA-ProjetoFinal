de3a768ac57e2097a14b0d9fa634b5b1013eec02
mariabcastro <72465450+mariabcastro@users.noreply.github.com>
2020-10-07 16:39:45 -0300

    Front end
    Front end

---
+++ estoque_ver.php
@@ -0,0 +1,26 @@
+
+
+<br><button id="btn_bscestoque">CONSULTAR ESTOQUE</button><br><br>
+
+<table>
+
+    <thead>
+
+        <tr>
+
+            <th>CÓD.</th>
+
+            <th>DESCRIÇÃO</th>
+
+            <th>QUANTIDADE</th>
+
+            <th>VALOR</th>      
+
+        </tr>
+
+    </thead>
+
+    <tbody id="tblestoque"></tbody>
+
+</table>
+
